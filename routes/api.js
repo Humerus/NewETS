6eca7347b47d2635efdfbebc173e2e932fef7a6e
Humerus <humerusj@gmail.com>
2015-05-23 19:41:34 -0400

    Added somethings

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,32 @@
 var request = require('request');
 var express = require('express');
 
-exports.api = function(req, res) {
-    
+exports.askDriver = function(req, res) {
+    request.post('http://textbelt.com/text').form({
+        number: req.query.number,
+        message: "Would you be able to drive a patient from " + req.query.fromaddress + " to " + req.query.toaddress + " at " + req.query.time + "? Please send a message saying YES or NO to: 12064306832"
+    });
+    res.status(200);
+    res.send({status: true});
+}
+
+exports.parseMessage = function(req, res) {
+    if(req.query.text && req.query.text.toLowerCase() == "yes") {
+        //forward req.query.msisdn
+        res.send({status: true});
+    }
+    res.status(200);
+    res.send({status: false});
+}
+
+exports.createNewResident = function(req, res) {
+
+}
+
+exports.createNewDriver = function(req, res) {
+
+}
+
+exports.logUserIn = function(req, res) {
+
 }
