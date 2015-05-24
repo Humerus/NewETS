5d0b76ec2b413784261ccbe8c89e22871540a922
Humerus <humerusj@gmail.com>
2015-05-24 08:16:26 -0400

    Cleaning

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('#page-content').pagify({
-        pages: ['home', 'about', 'signup', 'clients', 'managers', 'login'],
-        default: 'home',
-        animation: 'fadeIn'
-    });
-});
