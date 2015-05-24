badd685c157eee93fe1e393ce9d05d9652dba125
Humerus <humerusj@gmail.com>
2015-05-24 08:39:36 -0400

    Moreee

--- index.py
+++ index.py
@@ -200,6 +200,8 @@ def askDriver(number, fromAddress, toAddress, time):
 @app.route("/api/parseMessage", methods=['GET'])
 def parseMessage():
     text = request.args.get("text", "")
+    print str(text).lower()
+    print "yes"
     if str(text).lower() is "yes":
         print text
         tellDriver(number, "Thank you for accepting to help this person! If you have any other questions, please ask!")
